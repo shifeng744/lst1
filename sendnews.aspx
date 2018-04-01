bf4a08f8a817f39da283ac3136ccc58c052d698a
shifeng744 <627060296@qq.com>
2018-04-01 16:05:28 +0800

    s

---
+++ sendnews.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sendnews.aspx.cs" Inherits="NavShare.sendnews" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>发送</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>李胜涛 20150301215 男 2班</h3>
+        <input type="text" name="idtxt" />
+        <input type="submit" value="发送" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ sendnews.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Ad = Senparc.Weixin.MP.AdvancedAPIs;
+using Weixin;
+using Senparc.Weixin.MP.Entities;
+
+namespace NavShare
+{
+    public partial class sendnews : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string text = Request.Form["idtxt"];
+                string accessToken = Wx.accessToken;
+                //实验回复新闻消息
+                var l = Ad.User.List(accessToken, "");
+                string opid=l.data.openid.ToString();
+                if (text == opid)
+                {
+                    List<Article> News = new List<Article>();
+                    News.Add(new Article
+                    {
+                        PicUrl = "https://shifeng744.github.io/lst1/img/a3.jpg",
+                        Url = "https://shifeng744.github.io/lst1/index.aspx",
+                        Title = "李胜涛 男 20150301215"
+                    });
+                    News.Add(new Article
+                    {
+                        PicUrl = "https://shifeng744.github.io/lst1/img/a1.jpg",
+                        Description = "ViewNav页面",
+                        Url = "https://shifeng744.github.io/lst1/ViewNav.aspx",
+                        Title = "访问记录统计"
+                    });
+                    News.Add(new Article
+                        {
+                            PicUrl = "https://shifeng744.github.io/lst1/img/a2.jpg",
+                            Description = "ViewNav页面",
+                            Url = "https://shifeng744.github.io/lst1/ViewShare.aspx",
+                            Title = "访问记录统计"
+                        });
+                    
+                }
+                int n = 0;
+                foreach (var i in l.data.openid)
+                {
+                    try
+                    {
+                        CustomerService.SendText(accessToken, i, text);
+                        n++;
+                    }
+                    catch { }
+                }
+                ClientScript.RegisterStartupScript(GetType(), "k", string.Format("alert('成功向{0}个用户发送消息');", n), true);
+            }
+        }
+    }
+}
